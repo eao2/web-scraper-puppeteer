be3966c14cf2596d41846ac95d04f888bc8ad899
eao2 <enkh.amar.001@gmail.com>
2024-09-14 20:40:56 +0800

    t

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-    scrape();
-    res.send("scraping...")
-  });
+scrape();
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
